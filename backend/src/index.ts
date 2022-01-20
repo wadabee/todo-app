0af56c77da129cea155f7678a48c3340bf86fe26
Yusuke Wada <wada.ycx@om.asahi-kasei.co.jp>
2022-01-20 23:40:47 +0900

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+const app: express.Express = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//CROS対応（というか完全無防備：本番環境ではだめ絶対）
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+})
+
+app.listen(3000, () => {
+    console.log("Start on port 3000.")
+})
+
+type User = {
+    id: number
+    name: string
+    email: string
+};
+
+const users: User[] = [
+    { id: 1, name: "User1", email: "user1@test.local" },
+    { id: 2, name: "User2", email: "user2@test.local" },
+    { id: 3, name: "User3", email: "user3@test.local" }
+]
+
+//一覧取得
+app.get('/users', (req: express.Request, res: express.Response) => {
+    console.log('aa')
+    res.send(JSON.stringify(users))
+})
