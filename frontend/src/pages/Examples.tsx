a5a722e99494757a22d01a355caec42a97e4e5b1
Yusuke Wada <wada.ycx@om.asahi-kasei.co.jp>
2022-01-25 19:16:45 +0900

    add: swr

--- frontend/src/pages/Examples.tsx
+++ frontend/src/pages/Examples.tsx
@@ -1,7 +1,35 @@
+import { Box, Button, Card, CardActions, CardContent, Stack, Typography } from '@mui/material';
+import useSWR from 'swr';
+
 const Examples: React.FC = () => {
+  const fetcher = (url: string) => fetch(`http://localhost:8000${url}`).then((r) => r.json());
+
+  type User = {
+    id: string;
+    createdAt: string;
+    name: string;
+  };
+  const { data, error } = useSWR<User[]>('/users', fetcher);
+
   return (
     <>
       <h1>実装例</h1>
+
+      <Box sx={{ m: 3 }}>
+        <Stack>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                ユーザ一覧取得
+              </Typography>
+
+              <Typography variant="h5" component="div">
+                {!data ? 'データ未取得' : JSON.stringify(data)}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Stack>
+      </Box>
     </>
   );
 };
