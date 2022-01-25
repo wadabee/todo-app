ffe7ad474ec2a9be942d50ed596d4b055730d95a
Wadabee <bh.jpvw@gmail.com>
2022-01-25 23:46:27 +0900

    use shared type in frontend

--- frontend/src/pages/Examples.tsx
+++ frontend/src/pages/Examples.tsx
@@ -1,14 +1,9 @@
 import { Box, Button, Card, CardActions, CardContent, Stack, Typography } from '@mui/material';
 import useSWR from 'swr';
+import { User } from '@shared/types/User';
 
 const Examples: React.FC = () => {
   const fetcher = (url: string) => fetch(`http://localhost:8000${url}`).then((r) => r.json());
-
-  type User = {
-    id: string;
-    createdAt: string;
-    name: string;
-  };
   const { data, error } = useSWR<User[]>('/users', fetcher);
 
   return (
