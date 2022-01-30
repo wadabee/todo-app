4da6d5fb3623babbf40b00cf97514338d4f1e66c
Wadabee <bh.jpvw@gmail.com>
2022-01-31 01:53:45 +0900

    WIP: add userRepo test

--- backend/src/repository/__tests__/users.test.ts
+++ backend/src/repository/__tests__/users.test.ts
@@ -1,12 +1,42 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import UserRepo from '../users';
+import { prisma } from '../utils';
 
 describe('UserRepo', () => {
+  beforeAll(async () => {
+    await prisma.user.deleteMany();
+    await prisma.user.createMany({
+      data: [
+        {
+          id: 1,
+          name: 'test1',
+          createdAt: new Date('2022-01-30'),
+        },
+        {
+          id: 2,
+          name: 'test2',
+          createdAt: new Date('2022-01-30'),
+        },
+      ],
+    });
+  });
+
   const { getAllUsers } = UserRepo;
   describe('getAllUsers', () => {
     test('全ユーザが取得されること', async () => {
-      await expect(getAllUsers()).resolves.toEqual([]);
+      const actual = await getAllUsers();
+      expect(actual).toHaveLength(2);
+      expect(actual[0]).toEqual({
+        id: 1,
+        name: 'test1',
+        createdAt: new Date('2022-01-30'),
+      });
+      expect(actual[1]).toEqual({
+        id: 2,
+        name: 'test2',
+        createdAt: new Date('2022-01-30'),
+      });
     });
   });
 });
