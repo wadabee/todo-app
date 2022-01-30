b8b9926732928ce31fa48a9a7178df51201ffc3d
Wadabee <bh.jpvw@gmail.com>
2022-01-30 23:58:27 +0900

    add jest and unit test

---
+++ backend/src/repository/__tests__/users.test.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import UserRepo from '../users';
+
+describe('UserRepo', () => {
+  const { getAllUsers } = UserRepo;
+  describe('getAllUsers', () => {
+    test('全ユーザが取得されること', async () => {
+      await expect(getAllUsers()).resolves.toEqual('');
+    });
+  });
+});
