7519c7bfb2622c6767d500a549f9bc7cd66fa050
Wadabee <bh.jpvw@gmail.com>
2022-01-31 12:13:00 +0900

    DONE: add User repo test

---
+++ backend/__tests__/data/users.ts
@@ -0,0 +1,12 @@
+import { User } from '@prisma/client';
+
+export const USER_1: User = {
+  id: 1,
+  name: 'user1',
+  createdAt: new Date('2022-01-30'),
+};
+export const USER_2: User = {
+  id: 2,
+  name: 'user2',
+  createdAt: new Date('2022-01-30'),
+};
