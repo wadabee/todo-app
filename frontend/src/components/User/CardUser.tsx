00588ae719c4ceb56787f89aa58e5e5507f546ef
Wadabee <bh.jpvw@gmail.com>
2022-02-03 00:14:25 +0900

    user list to user card

---
+++ frontend/src/components/User/CardUser.tsx
@@ -0,0 +1,21 @@
+import { User } from '@backend/types/User';
+import { Card, CardContent, Typography } from '@mui/material';
+import React from 'react';
+
+type Props = {
+  user: User;
+};
+
+const CardUser: React.FC<Props> = ({ user }) => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h6" component="div" align="center">
+          {user.id}:{user.name}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CardUser;
--- frontend/src/components/User/CardUserList.tsx
+++ frontend/src/components/User/CardUserList.tsx
@@ -1,7 +1,8 @@
-import { Box, Card, CardContent, Typography } from '@mui/material';
+import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
 import React from 'react';
 import Loading from 'src/components/Loading';
 import useUser from 'src/hooks/useUser';
+import CardUser from './CardUser';
 
 export const CardUserList: React.FC = () => {
   const { getAllUsers } = useUser();
@@ -14,9 +15,25 @@ export const CardUserList: React.FC = () => {
           ユーザ一覧取得
         </Typography>
 
-        <Typography variant="h6" component="div" align="center">
-          {!data ? <Loading /> : JSON.stringify(data)}
-        </Typography>
+        <Box sx={{ mt: 3 }}>
+          {!data ? (
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+              <Grid item>
+                <Loading />
+              </Grid>
+            </Grid>
+          ) : (
+            <Grid container direction="row" spacing={3}>
+              {data.map
+                ? data.map((user) => (
+                    <Grid key={user.id} item md={3}>
+                      <CardUser user={user} />
+                    </Grid>
+                  ))
+                : ''}
+            </Grid>
+          )}
+        </Box>
       </CardContent>
     </Card>
   );
